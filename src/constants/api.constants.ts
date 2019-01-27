339011b1834c46f7b517a4a595b9ac0061da299e
Werner <werner@Werners-MBP.fritz.box>
2019-01-27 20:33:36 +0100

    new variables for the tv db

--- src/constants/api.constants.ts
+++ src/constants/api.constants.ts
@@ -7,17 +7,17 @@ export class API {
     public static URL_ENCODED_BOUQUET       = '1%3A7%3A1%3A0%3A0%3A0%3A0%3A0%3A0%3A0%3AFROM%20BOUQUET%20"userbouquet.appsender__tv_.tv"%20ORDER%20BY%20bouquet&day=0&epgmode=tv';
 
     //the movie db settings
-    public static MOVIEDB_API_KEY           = '44449b26f9060de49b1cbe419e8409e2';
+    public static MOVIEDB_API_KEY           = '';
     public static IMAGE_SIZE                = 'w300';
     public static MOVIEDB_API_PREFIX        = 'https://api.themoviedb.org/3/search/';
     public static MOVIEDB_API_IMAGE_PREFIX  = 'https://image.tmdb.org/t/p/';
 
     //the tv db settings
-    public static TVDB_API_KEY              = 'NCTB24ZYKGESPE9N';
-    public static TVDB_USERNAME             = 'Werner91vn3';
-    public static TVDB_USERKEY              = 'Z1FSQIL0FHKON3UK';
-    public static TVDB_PREFIX               = 'http://thetvdb.com/banners/';
-    public static TVDB_GET_TOKEN_URL        = 'https://api.thetvdb.com/login';
+    public static TVDB_API_KEY              = '';
+    public static TVDB_USERNAME             = '';
+    public static TVDB_USERKEY              = '';
+    public static TVDB_PREFIX               = '';
+    public static TVDB_GET_TOKEN_URL        = '';
     public static TVDB_TOKEN                = '';
 
     //images
